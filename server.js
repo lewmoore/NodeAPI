f3cb1567337a59485a807757d2bc92e34b187a87
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-09 13:37:51 +0100

    Add local MongoDB connection to API

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost:27017/citizens')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
