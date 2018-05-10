1e320191c2f8b5f2abacd6c01df4c8b8d46c1867
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-10 12:59:03 +0100

    Remove comments from code

--- test/database.test.js
+++ test/database.test.js
@@ -1,20 +1,12 @@
-"use strict";
-// NPM install mongoose and chai. Make sure mocha is globally
-// installed
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const chai = require('chai');
 const expect = chai.expect;
-// Create a new schema that accepts a 'name' object.
-// 'name' is a required field
 const testSchema = new Schema({
   name: { type: String, required: true }
 });
-//Create a new collection called 'Name'
 const ApplicantTest = mongoose.model('ApplicantTest', testSchema);
 describe('Database Tests', function() {
-  //Before starting the test, create a sandboxed database connection
-  //Once a connection is established invoke done()
   before(function (done) {
     mongoose.connect('mongodb://localhost:27017/testDatabase');
     const db = mongoose.connection;
@@ -25,7 +17,6 @@ describe('Database Tests', function() {
     });
   });
   describe('Test Database', function() {
-    //Save object with 'name' value of 'Mike"
     it('New name saved to test database', function(done) {
       var testName = ApplicantTest({
         name: 'Lewis'
@@ -34,7 +25,6 @@ describe('Database Tests', function() {
       testName.save(done);
     });
     it('Dont save incorrect format to database', function(done) {
-      //Attempt to save with wrong info. An error should trigger
       var wrongSave = ApplicantTest({
         notName: 'Not Lewis'
       });
@@ -44,7 +34,6 @@ describe('Database Tests', function() {
       });
     });
     it('Should retrieve data from test database', function(done) {
-      //Look up the 'Mike' object previously saved.
       ApplicantTest.find({name: 'Lewis'}, (err, name) => {
         if(err) {throw err;}
         if(name.length === 0) {throw new Error('No data!');}
@@ -52,7 +41,6 @@ describe('Database Tests', function() {
       });
     });
   });
-  //After all tests are finished drop database and close connection
   after(function(done){
     mongoose.connection.db.dropDatabase(function(){
       mongoose.connection.close(done);
